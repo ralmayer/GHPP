814206f1650bda30bc21219d68e5eb056214fe5f
Ral Mayer <llev4n@gmail.com>
2020-01-11 15:44:20 +0400

    tried some tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,64 @@
-import React, { useState, FunctionComponent as FC, FormEvent, ChangeEvent }  from "react";
-import { Counter } from './Counter'
+import React, {
+  useState,
+  FunctionComponent as FC,
+  FormEvent,
+  ChangeEvent
+} from "react";
+import { Counter } from "./Counter";
 
 const App: FC = () => {
-  
-  const [count, setCount] = useState<string>('')
-  const [title, setTitle] = useState<string>('')
-  const [values, setValues] = useState<{title: string, count: string }>({title: '', count: ''})
+  const [count, setCount] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [values, setValues] = useState<{ title: string; count: string }>({
+    title: "",
+    count: ""
+  });
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    setValues({title, count})
-  }
+    event.preventDefault();
+    setValues({ title, count });
+  };
 
   const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    setTitle(event.target.value)
-  }
+    event.preventDefault();
+    setTitle(event.target.value);
+  };
 
   const handleCountChange = (event: ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    setCount(event.target.value)
-  }
+    event.preventDefault();
+    setCount(event.target.value);
+  };
 
-  return <>
-  <form onSubmit={handleSubmit}>
-    <p>Enter Title: </p><input value={title} onChange={handleTitleChange}/>
-    <br />
-    <p>Enter Count: </p><input value={count} onChange={handleCountChange}/>
-    <button> SUBMIT </button>
-  </form>
-  <Counter title={values.title} count={values.count} />
-  </>
-
-}
+  return (
+    <div className="flex w-full h-screen justify-center items-center bg-gray-200">
+      <div className="container w-auto h-auto flex-initial bg-gray-500">
+        <form onSubmit={handleSubmit}>
+          <p className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 shadow-lg">
+            Enter Title:{" "}
+          </p>
+          <input
+            className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 shadow-lg"
+            value={title}
+            onChange={handleTitleChange}
+          />
+          <p className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 shadow-lg">
+            Enter Count:{" "}
+          </p>
+          <input
+            className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 shadow-lg"
+            value={count}
+            onChange={handleCountChange}
+          />
+          <br />
+          <button className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+            {" "}
+            SUBMIT{" "}
+          </button>
+        </form>
+        <Counter title={values.title} count={values.count} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -6,7 +6,7 @@ interface CouterProps {
 }
 
 export const Counter: FC<CouterProps> = ({title, count}) => <>
-    <h1>Title: {title}</h1>
+    <h1 className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">Title: {title}</h1>
     <br />
-    <h1>Price: {parseInt(count) * 2}</h1>
+    <h1 className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">Price: {count && parseInt(count) * 2}</h1>
 </>
